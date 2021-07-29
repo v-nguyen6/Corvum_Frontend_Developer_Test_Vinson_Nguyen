01035b43a1ed654908006cc380192ebabcb1b0ea
v-nguyen6 <73795871+v-nguyen6@users.noreply.github.com>
2021-07-29 14:12:45 -0700

    reinitialize to new folder and recreate react app with base completed

---
+++ corvum_test_breaking_bad_app/corvum_test_breaking_bad_app/src/App.js
@@ -0,0 +1,24 @@
+/*
+----------------------------------------------------------------
+General React Dependencies / Custom Imports & Components
+----------------------------------------------------------------
+*/
+// React Dependencies
+import React from 'react';
+import {useState, useEffect } from 'react';
+// Used to communicate with API's more easily
+//import axios from 'axios';
+
+
+//import './App.css';
+
+function App() {
+  return (
+    <div> 
+      <p> hey brosif</p>
+
+    </div>
+  );
+}
+
+export default App;
